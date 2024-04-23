a08d11b70132af2736ffd8fc58fc51f3a234dc69
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-04-23 17:55:48 -0400

    feat: commit solution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Smartphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
